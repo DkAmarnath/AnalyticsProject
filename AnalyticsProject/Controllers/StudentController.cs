844d8f2a9b0d55aebfadc062d3b41670fa847501
DkAmarnath <34921125+DkAmarnath@users.noreply.github.com>
2018-06-06 10:36:52 +0530

    aaa
    aa

---
+++ AnalyticsProject/Controllers/StudentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AnalyticsProject.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public string Index()
+        {
+            return "hello this is Amar";
+        }
+    }
+}
