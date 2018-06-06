3f6b4bee1579baf0136aed4689e638251f6b325f
DkAmarnath <34921125+DkAmarnath@users.noreply.github.com>
2018-06-06 09:11:12 +0530

    Initial commit

---
+++ AnalyticsProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AnalyticsProject.Startup))]
+namespace AnalyticsProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
