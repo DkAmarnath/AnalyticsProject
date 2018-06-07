686d6565f780a1d0055568c5151a663a9abdb801
DkAmarnath <34921125+DkAmarnath@users.noreply.github.com>
2018-06-08 02:22:15 +0530

    Student
    Added Student Controller and model and View

--- AnalyticsProject/Controllers/StudentController.cs
+++ AnalyticsProject/Controllers/StudentController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AnalyticsProject.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,26 @@ namespace AnalyticsProject.Controllers
     public class StudentController : Controller
     {
         // GET: Student
-        public string Index()
+        [ActionName("Hello")]//We can use actionname in url instead of actionMethod name.
+        //[NonAction]   //if you don't want to make a public method as actionmethod
+        public string Index1()
         {
             return "hello this is Amar";
+        }
+        public ActionResult Index()
+        {
+            var studentList = new List<Student>{ 
+                            new Student() { StudentId = 1, StudentName = "John", Age = 18 } ,
+                            new Student() { StudentId = 2, StudentName = "Steve",  Age = 21 } ,
+                            new Student() { StudentId = 3, StudentName = "Bill",  Age = 25 } ,
+                            new Student() { StudentId = 4, StudentName = "Ram" , Age = 20 } ,
+                            new Student() { StudentId = 5, StudentName = "Ron" , Age = 31 } ,
+                            new Student() { StudentId = 4, StudentName = "Chris" , Age = 17 } ,
+                            new Student() { StudentId = 4, StudentName = "Rob" , Age = 19 } 
+                        };
+            // Get the students from the database in the real application
+
+            return View(studentList);
         }
     }
 }
